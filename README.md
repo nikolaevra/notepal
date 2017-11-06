5d7eab46b0306b9fb619af41e397adaaa96a5c8e
nikolaevra <nikolaevra@gmail.com>
2017-11-06 13:49:21 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Creating Components
+===================
+in this part, we will create a basic application server, learn to bundle scripts using Webpack, get acquainted with basics of React (components, JSX syntax, props, states).
+# notepal
---
+++ server/models/index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Ruslan on 11/6/2017.
+ */
+const mongoose = require('mongoose');
+
+module.exports.connect = (uri) => {
+  mongoose.connect(uri);
+  // plug in the promise library:
+  mongoose.Promise = global.Promise;
+
+
+  mongoose.connection.on('error', (err) => {
+    console.error(`Mongoose connection error: ${err}`);
+    process.exit(1);
+  });
+
+  // load models
+  require('./user');
+};
